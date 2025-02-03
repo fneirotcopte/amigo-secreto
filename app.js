49e3382aa5af7111c2af179c431e50c4c526171e
Facundo <facuneirot1301@gmail.com>
2025-02-03 06:24:11 -0300

    crea funcion actualizarLista

--- app.js
+++ app.js
@@ -9,12 +9,16 @@ function agregarAmigo() {
         amigos.push(nombreIngresado);
         console.log(amigos);
         document.getElementById("amigo").value = ""; 
+        actualizaLista();
     }
 }
 
 function actualizaLista(){
-    document.getElementById("listaAmigos");
-    
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML= "";
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent= amigos[i];
+        lista.appendChild(li);
+    }
 }
-
-
